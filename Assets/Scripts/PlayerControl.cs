a82e179312109580afeefe81b5f9b3a7197291f3
aaaXorn <lcrs.rp@gmail.com>
2022-08-09 11:07:19 -0300

    rotacao WIP

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -11,19 +11,37 @@ public class PlayerControl : NetworkBehaviour
     [SerializeField]
     private float h_spd;
 
-    //camera
-    /*public override void OnStartLocalPlayer()
-    {
+    //rotation input
+    private Vector3 mouse_posDiff = Vector3.zero;//difference between current and previous mouse position
+    private Vector3 mouse_lastPos = Vector3.zero;//previous mouse position
+    //rotation speed
+    [SerializeField]
+    private float cam_x_spd, cam_y_spd;
 
-    }*/
+    //camera
+    public override void OnStartLocalPlayer()
+    {
+        mouse_lastPos = Input.mousePosition;
+    }
 
     private void Update()
     {
         //skips if object isn't owned by client
         if (!isLocalPlayer) return;
+        
+        //movement inputs
+        inputX = Input.GetAxis("Horizontal");
+        inputZ = Input.GetAxis("Vertical");
 
-        inputX = inputX.GetAxis("Horizontal");
-        inputZ = inputZ.GetAxis("Vertical");
+        //mouse inputs
+        mouse_posDiff = Input.mousePosition - mouse_lastPos;
+        mouse_lastPos = Input.mousePosition;
+
+        //rotation
+        if(mouse_posDiff != Vector3.zero)
+        {
+            transform.Rotate(0, mouse_posDiff.x * cam_x_spd * Time.deltaTime, 0);
+        }
     }
 
     private void FixedUpdate()
@@ -32,5 +50,6 @@ public class PlayerControl : NetworkBehaviour
         if (!isLocalPlayer) return;
 
         //movement
+
     }
 }
