6e6f293641661ae9eeab3ae9c12776be9e9d29c1
Xorn <lcrs.rp@gmail.com>
2022-09-15 16:43:19 -0300

    chat talvez funcional mas WIP

--- Assets/Scripts/UI/Chat.cs
+++ Assets/Scripts/UI/Chat.cs
@@ -16,8 +16,6 @@ public class Chat : NetworkBehaviour
     [SerializeField] private Text txt;
 	[SerializeField] private InputField iField;
 	
-	private static event Action<string> OnMessage;
-	
 	private void Awake()
 	{
         if(Instance == null) Instance = this;
@@ -31,22 +29,12 @@ public class Chat : NetworkBehaviour
 	public override void OnStartAuthority()
 	{
 		ChatBox.SetActive(true);
-		
-		OnMessage += HandleNewMessage;
-	}
-	
-	[ClientCallback]
-	private void OnDestroy()
-	{
-		if(!hasAuthority) return;
-		
-		OnMessage -= HandleNewMessage;
 	}
 	
 	private void HandleNewMessage(string msg)
 	{
 		string s = PN != null ? PN.nickname : "meucu";
-		txt.text += s + ": "+ msg;
+		txt.text += "\n" + s + ": "+ msg;
 		print("msg");
 	}
 	
@@ -56,7 +44,7 @@ public class Chat : NetworkBehaviour
 		if(!Input.GetKeyDown(KeyCode.Return)) return;
 		if(string.IsNullOrWhiteSpace(msg)) msg = iField.text;
 		
-		Cmd_SendMessage(iField.text);
+		if(!isServer) Cmd_SendMessage(iField.text); else SrvSendMessage(iField.text);
 		
 		iField.text = string.Empty;
 		print("fdp");
@@ -65,13 +53,20 @@ public class Chat : NetworkBehaviour
 	[Command]
 	private void Cmd_SendMessage(string msg)
 	{
-		Rpc_HandleMessage($"[{connectionToClient.connectionId}]: {msg}");
+		Rpc_HandleMessage(msg);
+		print("vsf");
+	}
+
+	private void SrvSendMessage(string msg)
+	{
+		Rpc_HandleMessage(msg);
 		print("vsf");
 	}
 	
 	[ClientRpc]
 	private void Rpc_HandleMessage(string msg)
 	{
-		OnMessage?.Invoke($"\n{msg}");
+		HandleNewMessage(msg);
+		print("rpc");
 	}
 }
